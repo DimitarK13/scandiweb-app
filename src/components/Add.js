2becbd32b4e53dbad379cf35eaea84fbfe3c76bb
Dimitar <dimikalapocev@gmail.com>
2023-02-08 17:25:36 +0100

    Added error handling for form as well as error messages

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Add() {
+  const [sku, setSku] = useState('');
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState(0);
+
   const switchType = (e) => {
     const selectedOption = e.target.value;
     const options = document.querySelectorAll('.option');
@@ -12,12 +17,45 @@ export default function Add() {
     document.getElementById(selectedOption).style.display = 'block';
   };
 
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    const validateForm = (value, name) => {
+      if (value === '' || value === 0 || value === undefined) {
+        document.querySelector(`.${name}Err`).textContent =
+          'Please specify this field';
+        return false;
+      } else {
+        document.querySelector(`.${name}Err`).textContent = '';
+        return true;
+      }
+    };
+
+    function pass() {
+      if (!validateForm(sku, 'sku')) return;
+      if (!validateForm(name, 'name')) return;
+      if (!validateForm(price, 'price')) return;
+
+      return true;
+    }
+
+    if (pass()) {
+      console.log('All Good');
+    }
+  };
+
   return (
     <div className='container'>
       <header>
         <h1>Product Add</h1>
         <div className='buttons'>
-          <button className='btn'>Save</button>
+          <button
+            onClick={handleForm}
+            form='product_form'
+            value='Submit'
+            className='btn'>
+            Save
+          </button>
           <Link to='/' className='btn'>
             Cancel
           </Link>
@@ -26,16 +64,41 @@ export default function Add() {
       <main>
         <form id='product_form'>
           <div className='form-item'>
-            <label htmlFor='sku'>SKU</label>
-            <input type='text' name='sku' id='sku' />
+            <label htmlFor='sku'>
+              SKU <span className='skuErr err'></span>
+            </label>
+            <input
+              type='text'
+              name='sku'
+              id='sku'
+              value={sku}
+              onChange={(e) => setSku(e.target.value)}
+            />
           </div>
           <div className='form-item'>
-            <label htmlFor='name'>Name</label>
-            <input type='text' name='name' id='name' />
+            <label htmlFor='name'>
+              Name <span className='nameErr err'></span>
+            </label>
+            <input
+              type='text'
+              name='name'
+              id='name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div className='form-item'>
-            <label htmlFor='price'>Price ($)</label>
-            <input type='text' name='price' id='price' />
+            <label htmlFor='price'>
+              Price ($) <span className='priceErr err'></span>
+            </label>
+            <input
+              type='number'
+              name='price'
+              id='price'
+              min='1'
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
           </div>
           <div className='form-item'>
             <label htmlFor='productType'>Type Switcher</label>
@@ -54,27 +117,27 @@ export default function Add() {
           <div id='DVD' className='option'>
             <div className='form-item'>
               <label htmlFor='size'>Size (MB)</label>
-              <input type='text' name='size' id='size' />
+              <input type='number' name='size' id='size' min='1' />
             </div>
           </div>
           <div id='Furniture' className='option'>
             <div className='form-item'>
               <label htmlFor='height'>Height (CM)</label>
-              <input type='text' name='height' id='height' />
+              <input type='number' name='height' id='height' min='1' />
             </div>
             <div className='form-item'>
               <label htmlFor='width'>Width (CM)</label>
-              <input type='text' name='width' id='width' />
+              <input type='number' name='width' id='width' min='1' />
             </div>
             <div className='form-item'>
               <label htmlFor='length'>Length (CM)</label>
-              <input type='text' name='length' id='length' />
+              <input type='number' name='length' id='length' min='1' />
             </div>
           </div>
           <div id='Book' className='option'>
             <div className='form-item'>
               <label htmlFor='weight'>Weight (KG)</label>
-              <input type='text' name='weight' id='weight' />
+              <input type='number' name='weight' id='weight' min='1' />
             </div>
           </div>
         </form>

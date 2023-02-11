d3eac1ddb7ab9f1139382fad8dcc63ab5ddf481a
Dimitar <dimikalapocev@gmail.com>
2023-02-11 19:45:51 +0100

    Fixed form a little bit and changed useStates for form elements

--- src/components/Add.js
+++ src/components/Add.js
@@ -2,9 +2,9 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Add() {
-  const [sku, setSku] = useState('');
-  const [name, setName] = useState('');
-  const [price, setPrice] = useState(0);
+  const [inputs, setInputs] = useState({});
+  const [attributes, setAttributes] = useState({});
+  const [fullForm, setFullForm] = useState({});
 
   const switchType = (e) => {
     const selectedOption = e.target.value;
@@ -12,36 +12,38 @@ export default function Add() {
 
     options.forEach((option) => {
       option.style.display = 'none';
+
+      let input_El = option.querySelectorAll('input');
+      input_El.forEach((input) => {
+        input.value = '';
+      });
     });
 
-    document.getElementById(selectedOption).style.display = 'block';
+    document.getElementById(selectedOption).style.display = 'flex';
+    setAttributes({});
+  };
+
+  const handleInputs = (e) => {
+    const { name, value } = e.target;
+
+    if (value) {
+      setInputs((prevValues) => ({ ...prevValues, [name]: value }));
+    }
+  };
+
+  const handleAttributes = (e) => {
+    const { name, value } = e.target;
+
+    if (value) {
+      setAttributes((prevValues) => ({ ...prevValues, [name]: value }));
+    }
   };
 
   const handleForm = (e) => {
     e.preventDefault();
 
-    const validateForm = (value, name) => {
-      if (value === '' || value === 0 || value === undefined) {
-        document.querySelector(`.${name}Err`).textContent =
-          'Please specify this field';
-        return false;
-      } else {
-        document.querySelector(`.${name}Err`).textContent = '';
-        return true;
-      }
-    };
-
-    function pass() {
-      if (!validateForm(sku, 'sku')) return;
-      if (!validateForm(name, 'name')) return;
-      if (!validateForm(price, 'price')) return;
-
-      return true;
-    }
-
-    if (pass()) {
-      console.log('All Good');
-    }
+    setFullForm({ ...inputs, ...attributes });
+    console.log(fullForm);
   };
 
   return (
@@ -63,82 +65,86 @@ export default function Add() {
       </header>
       <main>
         <form id='product_form'>
-          <div className='form-item'>
-            <label htmlFor='sku'>
-              SKU <span className='skuErr err'></span>
-            </label>
-            <input
-              type='text'
-              name='sku'
-              id='sku'
-              value={sku}
-              onChange={(e) => setSku(e.target.value)}
-            />
-          </div>
-          <div className='form-item'>
-            <label htmlFor='name'>
-              Name <span className='nameErr err'></span>
-            </label>
-            <input
-              type='text'
-              name='name'
-              id='name'
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className='form-item'>
-            <label htmlFor='price'>
-              Price ($) <span className='priceErr err'></span>
-            </label>
-            <input
-              type='number'
-              name='price'
-              id='price'
-              min='1'
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-            />
-          </div>
-          <div className='form-item'>
-            <label htmlFor='productType'>Type Switcher</label>
-            <select
-              type='text'
-              name='productType'
-              id='productType'
-              onChange={switchType}>
-              <option defaultChecked>Type Switcher</option>
-              <option value='DVD'>DVD</option>
-              <option value='Furniture'>Furniture</option>
-              <option value='Book'>Book</option>
-            </select>
-          </div>
+          <label htmlFor='sku'>
+            SKU <span className='skuErr err'></span>
+          </label>
+          <input type='text' name='sku' id='sku' onChange={handleInputs} />
+
+          <label htmlFor='name'>
+            Name <span className='nameErr err'></span>
+          </label>
+          <input type='text' name='name' id='name' onChange={handleInputs} />
+
+          <label htmlFor='price'>
+            Price ($) <span className='priceErr err'></span>
+          </label>
+          <input
+            type='number'
+            name='price'
+            id='price'
+            min='1'
+            onChange={handleInputs}
+          />
+
+          <label htmlFor='productType'>Type Switcher</label>
+          <select
+            type='text'
+            name='productType'
+            id='productType'
+            onChange={switchType}>
+            <option defaultChecked>Type Switcher</option>
+            <option value='DVD'>DVD</option>
+            <option value='Furniture'>Furniture</option>
+            <option value='Book'>Book</option>
+          </select>
 
           <div id='DVD' className='option'>
-            <div className='form-item'>
-              <label htmlFor='size'>Size (MB)</label>
-              <input type='number' name='size' id='size' min='1' />
-            </div>
+            <label htmlFor='size'>Size (MB)</label>
+            <input
+              type='number'
+              name='size'
+              id='size'
+              min='1'
+              onChange={handleAttributes}
+            />
           </div>
           <div id='Furniture' className='option'>
-            <div className='form-item'>
-              <label htmlFor='height'>Height (CM)</label>
-              <input type='number' name='height' id='height' min='1' />
-            </div>
-            <div className='form-item'>
-              <label htmlFor='width'>Width (CM)</label>
-              <input type='number' name='width' id='width' min='1' />
-            </div>
-            <div className='form-item'>
-              <label htmlFor='length'>Length (CM)</label>
-              <input type='number' name='length' id='length' min='1' />
-            </div>
+            <label htmlFor='height'>Height (CM)</label>
+            <input
+              type='number'
+              name='height'
+              id='height'
+              min='1'
+              onChange={handleAttributes}
+            />
+
+            <label htmlFor='width'>Width (CM)</label>
+            <input
+              type='number'
+              name='width'
+              id='width'
+              min='1'
+              onChange={handleAttributes}
+            />
+
+            <label htmlFor='length'>Length (CM)</label>
+            <input
+              type='number'
+              name='length'
+              id='length'
+              min='1'
+              onChange={handleAttributes}
+            />
           </div>
           <div id='Book' className='option'>
-            <div className='form-item'>
-              <label htmlFor='weight'>Weight (KG)</label>
-              <input type='number' name='weight' id='weight' min='1' />
-            </div>
+            <label htmlFor='weight'>Weight (KG)</label>
+            <input
+              type='number'
+              name='weight'
+              id='weight'
+              min='1'
+              onChange={handleAttributes}
+            />
           </div>
         </form>
       </main>
